0051910ac623500f84ddea1a7c292d32595e70e9
AlperCna <alpercn04@gmail.com>
2026-02-05 20:39:07 +0300

    Proje dosyası ekle.

---
+++ TodoApp.Domain/Entities/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TodoApp.Domain.Common;
+
+namespace TodoApp.Domain.Entities;
+
+public class TodoItem : BaseEntity
+{
+    public Guid UserId { get; set; }           // ownership için kritik
+    public string Title { get; set; } = default!;
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public DateTime? DueDate { get; set; }
+
+    // Navigation
+    public User? User { get; set; }
+}
---
+++ TodoApp.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Domain.Common;
+
+namespace TodoApp.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string Email { get; set; } = default!;
+    public string UserName { get; set; } = default!;
+
+    // Güvenlik: şifreyi plaintext tutmuyoruz
+    public string PasswordHash { get; set; } = default!;
+    public string PasswordSalt { get; set; } = default!;
+
+    // Navigation
+    public ICollection<TodoItem> TodoItems { get; set; } = new List<TodoItem>();
+}
